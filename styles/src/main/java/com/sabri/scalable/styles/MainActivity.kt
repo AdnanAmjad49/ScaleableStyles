d7c64930ccedee14ed7d2bb0db1aa757247832c8
Adnan Amjad <adnan.amjad.ravian@gmail.com>
2020-02-26 14:31:46 +0500

    initial commit

---
+++ styles/src/main/java/com/sabri/scalable/styles/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sabri.scalable.styles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
